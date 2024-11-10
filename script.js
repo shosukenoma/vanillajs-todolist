e7151638c44193a15159ceaddcd1489f48168b4f
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-11-09 20:46:48 -0600

    script.js messy first draft

---
+++ script.js
@@ -0,0 +1,19 @@
+const todoList = document.getElementById("todo-list")
+const addButton = document.getElementById("button-add")
+
+document.addEventListener(e) {
+  // if (e.element == button) {
+  //   addListItem()
+  // }
+}
+
+const addListItem = () => {
+  // todoList.appendElement(() => {
+  //   `
+  //   <li>
+  //     <p>${todoContent}</p>
+  //     <button>Delete</button>
+  //   </li>
+  //   `
+  // })
+}
