83876d603c9671b71a19030a54c5236ae1f024b8
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-11-10 21:23:36 -0600

    Decompose addNewItem functionality

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const textField = document.getElementById("textField")
 const buttonAdd = document.getElementById("buttonAdd")
 const todoList = document.getElementById("todoList")
 
+// If add button clicked
 buttonAdd.addEventListener("click", (e) => {
+  addNewItem(e);
+})
+
+// If enter key pressed down
+textField.addEventListener("keydown", (e) => {
+  if (e.key === 'Enter') {
+    addNewItem(e);
+  }
+})
+
+const addNewItem = (e) => {
   e.preventDefault();
   // Make sure to defer script.js in html.
   // getElementById is executed before DOM finishes loading.
@@ -13,7 +25,6 @@ buttonAdd.addEventListener("click", (e) => {
     <button type="button" id="buttonDelete">Delete</button>
   </li>
   `;
-
   todoList.insertAdjacentHTML("beforeend", newItemHTML);
   textField.value = "";
-})
+}
