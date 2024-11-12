3577f36810e2bbc4b2bac1e4ac4c5cf6d1af7677
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2024-11-12 17:38:49 -0600

    Implement delete function using "e.target.classList.contains()", "removeChild", and "parentNode"

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
+// Make sure to defer script.js in html.
+  // Otherwise getElementById is executed before DOM finishes loading.
+  // -> Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')
+const todoList = document.getElementById("todoList")
 const textField = document.getElementById("textField")
 const buttonAdd = document.getElementById("buttonAdd")
-const todoList = document.getElementById("todoList")
 
+// === ADD (Click) ===
 // If add button clicked
-buttonAdd.addEventListener("click", (e) => {
+buttonAdd.addEventListener('click', (e) => {
   addNewItem(e);
 })
 
+// === ADD (Enter) ===
 // If enter key pressed down
-textField.addEventListener("keydown", (e) => {
+textField.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     addNewItem(e);
   }
 })
 
+// === DELETE ===
+// USE EVENT DELEGATION. Do not add event listener to each "delete item" button.
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains("btn--delete")) {
+    console.log(e.target.parentNode);
+    todoList.removeChild(e.target.parentNode)
+  }
+})
+
+// === ADD Helper ===
 const addNewItem = (e) => {
   e.preventDefault();
-  // Make sure to defer script.js in html.
-  // getElementById is executed before DOM finishes loading.
-  // -> Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')
   let newItemHTML = `
   <li>
     <p>${textField.value}</p>
-    <button type="button" id="buttonDelete">Delete</button>
+    <button type="button" class="btn btn--delete">Delete</button>
   </li>
   `;
   todoList.insertAdjacentHTML("beforeend", newItemHTML);
